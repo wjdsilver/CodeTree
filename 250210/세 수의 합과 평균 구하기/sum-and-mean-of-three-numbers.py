977567299eee0c7f0fe295df9c32a0dfdbc0a39a
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-10 16:12:59 +0900

    Title: 세 수의 합과 평균 구하기, Time: 56ms, Memory: 16MB, Status: Passed - CodeTree

--- 250210/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
+++ 250210/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -1,6 +1,7 @@
 arr=input().split()
-a=int(arr[0])
-b=int(arr[1])
-c=int(arr[2])
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
 
-print(f"{a+b+c}\n{(a+b+c)/3:.0f}")
+print(a + b + c)
+print((a + b + c) // 3)
