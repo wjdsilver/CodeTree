2f652cb29b0e18ea72fdcd1f9d5308e55cb88a7d
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-11 15:51:22 +0900

    Title: 코로나 메뉴얼, Time: 53ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250211/코로나 메뉴얼/covid-manual.py
+++ 250211/코로나 메뉴얼/covid-manual.py
@@ -11,12 +11,10 @@ p3_ill=arr[0]
 p3_temp=int(arr[1])
 
 if p1_ill=="Y" and p1_temp>=37:
-    if p2_ill=="Y" and p2_temp>=37:
-        print("E")
-    if p3_ill=="Y" and p3_temp>=37:
-        print("E")
-elif p2_ill=="Y" and p2_temp>=37:
-    if p3_ill=="Y" and p3_temp>=37:
+    if (p2_ill=="Y" and p2_temp>=37) or(p3_ill=="Y" and p3_temp>=37):
         print("E")
 else:
-    print("N")
+    if (p2_ill=="Y" and p2_temp>=37) and (p3_ill=="Y" and p3_temp>=37):
+        print("E")
+    else:
+        print("N")
