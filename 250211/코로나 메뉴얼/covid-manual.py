6821a024565cb8cab5190f48005258e308d6d6c8
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-11 15:48:23 +0900

    Title: 코로나 메뉴얼, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250211/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,22 @@
+arr=input().split()
+p1_ill=arr[0]
+p1_temp=int(arr[1])
+
+arr=input().split()
+p2_ill=arr[0]
+p2_temp=int(arr[1])
+
+arr=input().split()
+p3_ill=arr[0]
+p3_temp=int(arr[1])
+
+if p1_ill=="Y" and p1_temp>=37:
+    if p2_ill=="Y" and p2_temp>=37:
+        print("E")
+    if p3_ill=="Y" and p3_temp>=37:
+        print("E")
+elif p2_ill=="Y" and p2_temp>=37:
+    if p3_ill=="Y" and p3_temp>=37:
+        print("E")
+else:
+    print("N")
