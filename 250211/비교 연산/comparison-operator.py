5a985c063ee2f049167ebede96061d5da5bd9cab
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-11 14:28:50 +0900

    Title: 비교 연산, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250211/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+arr=input().split()
+a=int(arr[0])
+b=int(arr[1])
+if a>=b:
+    if True:
+        print("1")
+else:
+    print("0")
+if a>b:
+    if True:
+        print("1")
+else:
+    print("0")
+if b>=a:
+    if True:
+        print("1")
+else:
+    print("0")
+if b>a:
+    if True:
+        print("1")
+else:
+    print("0")
+if a==b:
+    if True:
+        print("1")
+else:
+    print("0")
+if a!=b:
+    if True:
+        print("1")
+else:
+        print("0")
