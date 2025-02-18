eaed92f76c9fd7725af7bc0afd6c1cd2398898b4
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-18 21:45:56 +0900

    Title: 배수의 개수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250218/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,11 @@
+cnt3=0
+cnt5=0
+for _ in range (10):
+    n=int(input())
+    if n%3==0:
+        cnt3+=1
+        if n%5==0:
+            cnt5+=1
+    elif n%5==0:
+        cnt5+=1
+print(cnt3,cnt5)
