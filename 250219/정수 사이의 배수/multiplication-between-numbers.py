4fa366b7dddc74e2dccccc7a461de2ac1b2080a7
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-19 09:22:25 +0900

    Title: 정수 사이의 배수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250219/정수 사이의 배수/multiplication-between-numbers.py
+++ 250219/정수 사이의 배수/multiplication-between-numbers.py
@@ -2,7 +2,9 @@ arr=input().split()
 a=int(arr[0])
 b=int(arr[1])
 int_sum=0
+cnt=0
 for i in range (a,b+1):
     if i%5==0 or i%7==0:
         int_sum+=i
-print(f"{int_sum} {int_sum/(b-a+1):.1f}")
+        cnt+=1
+print(f"{int_sum} {int_sum/cnt:.1f}")
