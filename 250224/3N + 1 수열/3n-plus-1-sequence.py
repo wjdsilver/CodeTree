30e951e7c80ff7fb05f63fde0425322b6be4aa93
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-02-24 17:45:24 +0900

    Title: 3N + 1 수열, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250224/3N + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,15 @@
+n=int(input())
+cnt=0
+while True:
+    if n==1:
+        print(cnt)
+        break
+    if n%2==0:
+        n/=2
+        cnt+=1
+    elif n%2==1:
+        n=n*3+1
+        cnt+=1
+    
+        
+    
