313fa8c2c853b7b29f28d61dab48929063a7d3b2
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-03-10 15:08:17 +0900

    Title: 별표 출력하기 10, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250310/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,13 @@
+n=int(input())
+m=1
+for i in range(1,2*n+1):
+    if i%2==0:
+        for j in range(n):
+            print("* ",end="")
+        n-=1
+        print()
+    elif i%2==1:
+        for j in range(m):
+            print("* ",end="")
+        m+=1
+        print()
