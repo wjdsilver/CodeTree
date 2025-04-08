ec1b9b33aec64c8919b44778598ac739a3c6bc50
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-04-08 11:50:02 +0900

    Title: 구구단 만들기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250408/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,9 @@
+arr=input().split()
+a=int(arr[0])
+b=int(arr[1])
+for i in range (1,10):
+    for j in range (b,a-1,-2):
+        print(f"{j} * {i} = {i * j}", end="")
+        if a<j:
+            print(" /", end=" ") 
+    print()
