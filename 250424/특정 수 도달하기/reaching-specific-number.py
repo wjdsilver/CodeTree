86ed3685c3e12013197362f569e2a4740661c2a7
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-04-24 15:53:25 +0900

    Title: 특정 수 도달하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250424/특정 수 도달하기/reaching-specific-number.py
+++ 250424/특정 수 도달하기/reaching-specific-number.py
@@ -4,9 +4,12 @@ sum_val=0
 cnt=0
 
 for i in range (n):
-    if arr[i]<250:
-        sum_val+=arr[i]
-        cnt+=1
-    elif arr[i]>=250:
+    if i==10:
         print(f"{sum_val} {sum_val/cnt:.1f}")
-        break
+    else:
+        if arr[i]<250:
+            sum_val+=arr[i]
+            cnt+=1
+        elif arr[i]>=250:
+            print(f"{sum_val} {sum_val/cnt:.1f}")
+            break
