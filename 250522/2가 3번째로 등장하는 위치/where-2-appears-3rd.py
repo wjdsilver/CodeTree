88e84a9a8a19b68c1cf7c6d216b9e98a2ff3a3b6
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-05-22 15:41:25 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250522/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().split()))
+cnt=0
+
+for i in range (len(arr)):
+    if arr[i]==2:
+        cnt+=1
+        if cnt==3:
+            print(i+1)
+            break
