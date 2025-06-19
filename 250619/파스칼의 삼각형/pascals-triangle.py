47602a110175717d4e81cc9f857705f0af37243e
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-06-19 14:51:01 +0900

    Title: 파스칼의 삼각형, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/파스칼의 삼각형/pascals-triangle.py
+++ 250619/파스칼의 삼각형/pascals-triangle.py
@@ -4,18 +4,16 @@ arr_2d = [
     for _ in range(n)
 ]
 
-arr_2d[0][0]=1
-arr_2d[1][0]=1
-arr_2d[1][1]=1
-
-for i in range (2,n):
-    for j in range(n):
-        arr_2d[i][j] = arr_2d[i-1][j-1] +arr_2d[i-1][j] 
-
+for i in range(n):
+    arr_2d [i][0] = 1
+    arr_2d [i][i] = 1
 
 for i in range(n):
-	for j in range(i + 1):
-		print(arr_2d[i][j], end=" ")
-	print()
-
-
+    for j in range(1, i):
+            arr_2d [i][j] = arr_2d [i-1][j-1] + arr_2d [i-1][j]
+        
+        
+for i in range(n):
+    for j in range(i + 1):
+        print(arr_2d [i][j], end=' ')
+    print()
