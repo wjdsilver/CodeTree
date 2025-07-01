2881eb63ec071b59e1a0969c5e775979076a7543
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-01 17:48:16 +0900

    Title: 2021년 날짜의 유무, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250701/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,33 @@
+def feb(D):
+    if D<=28:
+        return True
+    else:
+        return False
+
+
+def M30(D):
+    if D<=30:
+        return True
+    else:
+        return False
+
+
+def M31(D):
+    if D<=31:
+        return True
+    else:
+        return False
+
+
+M, D = map(int, input().split())
+if M==2:
+    result=feb(D)
+elif M==4 or M==6 or M==9 or M==11:
+    result=M30(D)
+else:
+    result=M31(D)
+
+if result==True:
+    print("Yes")
+else:
+    print("No")
