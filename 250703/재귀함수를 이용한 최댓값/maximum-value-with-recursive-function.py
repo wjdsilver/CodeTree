de24f472640a98d3511744eed37546d276673c43
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-03 11:29:14 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250703/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250703/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -4,8 +4,6 @@ arr = list(map(int, input().split()))
 def f(n):
     if n==0:
         return arr[0]
-    elif n==1:
-        return arr[1]
-    return max(f(n-2),arr[n-1])
+    return max(f(n-1),arr[n])
 
-print(f(n))
+print(f(n-1))
