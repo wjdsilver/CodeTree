839d37ad4b098d00b54d1e243762148c83cf44b9
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-08 16:18:32 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250708/그 요일은/the-day-of-the-day.py
+++ 250708/그 요일은/the-day-of-the-day.py
@@ -2,6 +2,7 @@ m1, d1, m2, d2 = map(int, input().split())
 A=input()
 day=[0,31,29,31,30,31,30,31,31,30,31,30,31]
 w_day=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
 def Days(x,y):
     result=0
     for i in range(x):
@@ -10,7 +11,7 @@ def Days(x,y):
     return result
 w=w_day.index(A)
 
-if Days(m2,d2)-Days(m1,d1)%7>w:
+if (Days(m2,d2)-Days(m1,d1))%7>=w:
     print((Days(m2,d2)-Days(m1,d1))//7+1)
 else:
     print((Days(m2,d2)-Days(m1,d1))//7)
