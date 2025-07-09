041982ba41d94dbc67748a9d7d1e0ef73c098f30
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-09 17:28:46 +0900

    Title: 흰검 칠하기, Time: 82ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250709/흰검 칠하기/painting-white-black.py
+++ 250709/흰검 칠하기/painting-white-black.py
@@ -11,27 +11,33 @@ for _ in range(n):
 
     if di=="L":
         for j in range(cur-x+1,cur+1):
-            if w[j]==0:
-                w[j]=1
-                g[j]=1
-            elif w[j]==1:
-                if b[j]>=2:
-                    g[j]=3
-                else:
-                    w[j]=2
+            if g[j]==3:
+                continue
+            else:
+                if w[j]==0:
+                    w[j]=1
                     g[j]=1
+                elif w[j]==1:
+                    if b[j]>=2:
+                        g[j]=3
+                    else:
+                        w[j]=2
+                        g[j]=1
         cur=cur-x+1
     elif di=="R":
         for j in range(cur,cur+x):
-            if b[j]==0:
-                b[j]=1
-                g[j]=2
-            elif b[j]==1:
-                if w[j]>=2:
-                    g[j]=3
-                else:
-                    b[j]=2
+            if g[j]==3:
+                continue
+            else:
+                if b[j]==0:
+                    b[j]=1
                     g[j]=2
+                elif b[j]==1:
+                    if w[j]>=2:
+                        g[j]=3
+                    else:
+                        b[j]=2
+                        g[j]=2
         cur=cur+x-1
 
 for i in range(2001):
