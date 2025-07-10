7d00be78d05ba0bdab2ea7a954af3a0d6b6e21d9
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-10 17:26:29 +0900

    Title: 연속되는 수 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250710/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+ans,cnt=0,0
+
+for i in range(1,n):
+    if arr[i-1]*arr[i]>0:
+        cnt+=1
+    elif arr[i-1]*arr[i]<0:
+        if ans<cnt:
+            ans=cnt
+        cnt=1
+print(ans)
