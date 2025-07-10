bd31a17ccd90c93d57beeb40af1f667aeec94357
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-10 11:12:51 +0900

    Title: 흰검 칠하기, Time: 81ms, Memory: 22MB, Status: Passed - Codetree

--- 250710/흰검 칠하기/painting-white-black.py
+++ 250710/흰검 칠하기/painting-white-black.py
@@ -1,43 +1,44 @@
+MAX_K = 100000
+# 변수 선언 및 입력:
 n = int(input())
-WHITE, BLACK, GRAY = 1, 2, 3
-size = 200001
-offset = 100000
+a = [0] * (2 * MAX_K + 1)
+cnt_b = [0] * (2 * MAX_K + 1)
+cnt_w = [0] * (2 * MAX_K + 1)
+b, w, g = 0, 0, 0
 
-# 각 색 횟수 카운트 배열
-white = [0] * size
-black = [0] * size
-color = [0] * size  # 현재 타일 상태 (1=흰, 2=검, 3=회)
-
-cur = offset
-
+cur = MAX_K
 for _ in range(n):
-    x, d = input().split()
+    x, c = tuple(input().split())
     x = int(x)
 
-    if d == "L":
-        for i in range(cur, cur - x, -1):
-            if color[i] == GRAY:
-                continue
-            white[i] += 1
-            if white[i] >= 2 and black[i] >= 2:
-                color[i] = GRAY
-            elif color[i] != BLACK:
-                color[i] = WHITE
-        cur = cur - x
-    else:  # "R"
-        for i in range(cur, cur + x):
-            if color[i] == GRAY:
-                continue
-            black[i] += 1
-            if white[i] >= 2 and black[i] >= 2:
-                color[i] = GRAY
-            elif color[i] != WHITE:
-                color[i] = BLACK
-        cur = cur + x - 1
+    if c == 'L':
+        # x칸 왼쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 1
+            cnt_w[cur] += 1
+            x -= 1
 
-# 최종 색상 카운트
-w = color.count(WHITE)
-b = color.count(BLACK)
-g = color.count(GRAY)
+            if x: 
+                cur -= 1
+    else:
+        # x칸 오른쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 2
+            cnt_b[cur] += 1
+            x -= 1
 
+            if x: 
+                cur += 1
+
+for i in range(2 * MAX_K + 1):
+    # 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+    if cnt_b[i] >= 2 and cnt_w[i] >= 2: 
+        g += 1
+    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+    elif a[i] == 1: 
+        w += 1
+    elif a[i] == 2: 
+        b += 1
+
+# 정답을 출력합니다.
 print(w, b, g)
