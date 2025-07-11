c6c1a566821d11f65180a40b42ecd723408b6357
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-11 10:55:26 +0900

    Title: 연속되는 수 4, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250711/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+ans,cnt=1,0
+
+for i in range(1,n):
+    if arr[i-1]<arr[i]:
+        cnt+=1
+    elif arr[i-1]>=arr[i]:
+        if ans<cnt:
+            ans=cnt
+        cnt=1
+if ans<cnt:
+    cnt+=1
+    print(cnt)
+else:
+    print(ans)
