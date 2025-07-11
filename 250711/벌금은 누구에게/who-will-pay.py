44cc93014be70c9de379cbc4699e45de93ef3038
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-11 17:35:17 +0900

    Title: 벌금은 누구에게, Time: 109ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250711/벌금은 누구에게/who-will-pay.py
+++ 250711/벌금은 누구에게/who-will-pay.py
@@ -5,8 +5,12 @@ arr=[0]*(N+1)
 for s in range(M):
     s=int(input())
     arr[s]+=1
-    if arr[s]==K:
-        print(s)
+for i in range(M):
+    ans=-1
+    if arr[i]==K:
+        ans=i
         break
+print(ans)
+
             
 
