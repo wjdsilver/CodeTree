0a1a51f12e5c7bbe88b62bef154e2a85869c7f58
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-11 17:36:50 +0900

    Title: 벌금은 누구에게, Time: 77ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250711/벌금은 누구에게/who-will-pay.py
+++ 250711/벌금은 누구에게/who-will-pay.py
@@ -1,12 +1,11 @@
 N, M, K = map(int, input().split())
 arr=[0]*(N+1)
-
+ans=-1
 
 for s in range(M):
     s=int(input())
     arr[s]+=1
-for i in range(M):
-    ans=-1
+for i in range(N):
     if arr[i]==K:
         ans=i
         break
