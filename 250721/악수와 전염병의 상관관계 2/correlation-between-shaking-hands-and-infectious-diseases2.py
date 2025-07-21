d4328c450332fc8ba4b6476217de268ce1aa9b9d
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-21 15:24:45 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250721/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,30 @@
+N, K, P, T = map(int, input().split())
+hands=[] #입력받은거 저장
+ans=[0]*(N+1) #감염 결과 저장
+cnt=[0]*(N+1) #감염 횟수 저장
+
+class Hand:
+    def __init__(self,t,x,y):
+        self.t=t
+        self.x=x
+        self.y=y
+
+
+for _ in range(T):
+    t, x, y = map(int, input().split())
+    hands.append(Hand(t, x, y))
+hands.sort(key=lambda x:x.t)
+ans[P]=1
+for h in hands:
+    x,y=h.x,h.y
+    if ans[x]==1 and cnt[x]<K:
+        if not ans[y]:
+            ans[y]=1
+        cnt[x]+=1
+    elif ans[y]==1 and cnt[y]<K:
+        if not ans[x]:
+            ans[x]=1
+        cnt[y]+=1
+
+for i in range (1,N+1):
+    print(ans[i],end="")
