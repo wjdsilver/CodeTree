eee635a2f70a2678545a471267a65899c3591d4b
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-21 12:53:57 +0900

    Title: 선두를 지켜라, Time: 108ms, Memory: 30MB, Status: Passed - Codetree

---
+++ 250721/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+arr_a=[]
+arr_b=[]
+
+p=0
+for _ in range(n):
+    v, t = map(int, input().split())
+    for i in range(t):
+            p += v
+            arr_a.append(p)
+
+p=0
+for _ in range(m):
+    v, t = map(int, input().split())
+    for i in range(t):
+            p += v
+            arr_b.append(p)
+        
+
+T = min(len(arr_a), len(arr_b))
+
+change = 0
+prev = 0
+
+for i in range(T):
+    if arr_a[i] > arr_b[i]:
+        curr= 1
+    elif arr_a[i] < arr_b[i]:
+        curr= 2
+    else:
+        curr= 0
+
+    if curr != 0 and curr != prev:
+        change += 1
+        prev = curr
+    elif curr != 0:
+        prev = curr
+
+print(change-1)
