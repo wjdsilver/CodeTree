d82ee33aef1866f62c77a48d8c49c32f744a828c
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-21 13:57:13 +0900

    Title: 좌우로 움직이는 로봇, Time: 231ms, Memory: 53MB, Status: Passed - Codetree

--- 250721/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250721/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -29,15 +29,15 @@ minT = min(len(arr_a), len(arr_b))
 maxT = max(len(arr_a), len(arr_b))
 cnt=0
 
-for i in range(1,minT+1):
+for i in range(1,minT):
     if arr_a[i-1] != arr_b[i-1] and arr_a[i] == arr_b[i] :
         cnt+=1
-if minT==len(arr_a):
-    for j in range(minT+1,maxT-1):
+if len(arr_a)<len(arr_b):
+    for j in range(minT,maxT-1):
         if arr_a[minT-1]!=arr_b[j-1] and arr_a[minT-1]==arr_b[j] :
             cnt+=1
-else:
-    for j in range(minT+1,maxT-1):
+elif len(arr_a)>len(arr_b):
+    for j in range(minT,maxT-1):
         if arr_b[minT-1]!=arr_a[j-1] and arr_b[minT-1]==arr_a[j] :
             cnt+=1
 print(cnt)
