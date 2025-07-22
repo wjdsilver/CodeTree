12a66e811bd44d16886b4639b333207484772a48
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-22 16:55:46 +0900

    Title: 작은 구슬의 이동, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250722/작은 구슬의 이동/small-marble-movement.py
+++ 250722/작은 구슬의 이동/small-marble-movement.py
@@ -1,6 +1,8 @@
 n, t = map(int, input().split())
 r, c, d = input().split()
 x, y = int(r), int(c)
+x=x-1
+y=y-1
 leftT=t
 
 dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
@@ -16,7 +18,7 @@ move_dir = mapper[d]
 
 
 def in_range(x, y):
-    return 0 < x < n and 0 < y < n
+    return 0 <= x < n and 0 <= y < n
 
 while leftT:
     nx, ny = x + dxs[move_dir], y + dys[move_dir]
@@ -27,6 +29,6 @@ while leftT:
         move_dir = 3 - move_dir
     leftT -= 1
     
-print(x,y)
+print(x+1,y+1)
 
 
