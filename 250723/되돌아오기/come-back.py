c723d419d5b982795c4f1ea245c67e5b0dd8225c
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-23 13:50:22 +0900

    Title: 되돌아오기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250723/되돌아오기/come-back.py
@@ -0,0 +1,30 @@
+N = int(input())
+
+t=0
+ans=-1
+x,y=0,0
+
+dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
+
+mapper = {
+    'N': 0,
+    'E': 1,
+    'W': 2,
+    'S': 3
+}
+
+
+for i in range(N):
+    w,d=input().split()
+    d=int(d)
+    move_dir = mapper[w]
+    for _ in range(d):
+        x,y=x+dxs[move_dir],y+dys[move_dir]
+        t+=1
+        if x==0 and y==0:
+            ans=t
+            break
+        
+print(ans)
+
+    
