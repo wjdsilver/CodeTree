d2a70e9fd8d9125fde48ab8617315bc836b72a02
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-25 15:03:23 +0900

    Title: 체크판위에서 2, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250725/체크판위에서 2/on-the-checkboard-2.py
+++ 250725/체크판위에서 2/on-the-checkboard-2.py
@@ -1,18 +1,16 @@
 R, C = map(int, input().split())
 arr = [list(input().split()) for _ in range(R)]
 cnt=0
-
-for i in range(R-2):
-    for j in range(C-2):
-        for k in range(i + 1, R-1):
-            for l in range(j + 1,C-1):
-                if arr[0][0]=="W":
-                    if arr[i][j]=="B":
-                        if arr[k][l]=="W":
+if arr[0][0]!=arr[R-1][C-1]:
+    for i in range(R-2):
+        for j in range(C-2):
+            for k in range(i + 1, R-1):
+                for l in range(j + 1,C-1):
+                    if arr[0][0]=="W":
+                        if arr[i][j]=="B" and arr[k][l]=="W":
                             cnt+=1
-                elif arr[0][0]=="B":
-                    if arr[i][j]=="W":
-                        if arr[k][l]=="B":
+                    elif arr[0][0]=="B":
+                        if arr[i][j]=="W" and arr[k][l]=="B":
                             cnt+=1
                         
 print(cnt)
