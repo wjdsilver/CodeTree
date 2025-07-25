dcd8773361ff2759e1fc4163da643a561dc822f1
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-25 13:29:13 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250725/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250725/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,26 +1,30 @@
+import sys
 n = int(input())
 grid = [[0] * n for _ in range(n)]
 
 r,c=n//2,n//2
-cnt, d=1, 0
+cnt, step=1, 1
 grid[r][c]=cnt
 
 def inRange(r,c):
     return 0<=r<n and 0<=c<n
 
-dxs,dys=[1,0,-1,0],[0,1,0,-1]
+dxs = [0, -1, 0, 1]
+dys = [1, 0, -1, 0]
+d = 0 
 
-while 1 <= cnt <= n*n:
-    d=(d+1)%4
-    nr,nc=r+step*dxs[d],c+step*dys[d]
-    print(nr,nc,d,cnt)
-    if not inRange(nr,nc) or grid[nr][nc]!=0:
-        d=(d+1)%4
-        nr,nc=r+dxs[d],c+dys[d]
-    r,c=nr,nc
-    
-    grid[r][c]=cnt
-    if cnt%2==1:
-        d=(d+4-1)%4
-for row in grid:
-    print(*row)
+while cnt < n*n:
+    for _ in range(2):  # 두 번씩 반복
+        for _ in range(step):
+            r += dxs[d]
+            c += dys[d]
+            cnt += 1
+            grid[r][c] = cnt
+            if cnt==n*n:
+                for row in grid:
+                    print(*row)
+                sys.exit() 
+        d = (d + 1) % 4 
+    step += 1
+        
+
