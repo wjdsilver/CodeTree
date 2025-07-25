e13ad55e2c8e80ded0780d9a8607bbcc19d20f06
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-25 15:17:37 +0900

    Title: 이상한 진수 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250725/이상한 진수 2/awkward-digits-2.py
+++ 250725/이상한 진수 2/awkward-digits-2.py
@@ -1,9 +1,12 @@
 a = input()
 na=""
 ans=0
-if a[1]=="0":
-    na="11"+a[2:]
-elif a[1]=="1":
-    na="10"+a[2:]
-ans = int(na, 2)
-print(ans)
+if len(a)>1:
+    if a[1]=="0":
+        na="11"+a[2:]
+    elif a[1]=="1":
+        na="10"+a[2:]
+    ans = int(na, 2)
+    print(ans)
+else:
+    print(0)
