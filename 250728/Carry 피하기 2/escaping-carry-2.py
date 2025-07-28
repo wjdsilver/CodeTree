bb6e0c402a2fad1ee1832c01fd8010a07d100c45
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-28 15:51:42 +0900

    Title: Carry 피하기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250728/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,27 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+fin=0
+ans=0
+
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            a=arr[i]
+            b=arr[j]
+            c=arr[k]
+            ans=a+b+c
+            notcarry=True
+            while a+b>=10 or b+c>=10 or c+a>=10:
+                if a%10+b%10+c%10>10:
+                    notcarry=False
+                    break
+                a//=10
+                b//=10
+                c//=10
+                
+            if notcarry:
+                fin=max(fin,ans)
+print(fin)
+                
+
+            
