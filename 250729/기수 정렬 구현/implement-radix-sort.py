9873947f1394b0df9b4d1d0e232cb42e63d2285a
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-29 16:43:09 +0900

    Title: 기수 정렬 구현, Time: 99ms, Memory: 26MB, Status: Passed - Codetree

--- 250729/기수 정렬 구현/implement-radix-sort.py
+++ 250729/기수 정렬 구현/implement-radix-sort.py
@@ -10,11 +10,11 @@ def radix_sort(arr):
             digit = (arr[i] // (10 ** pos)) % 10
             arr_new[digit].append(arr[i])
 
-    store_arr = []
-    for i in range(10):
-        for j in range(len(arr_new[i])):
-            store_arr.append(arr_new[i][j])
-    arr = store_arr
+        store_arr = []
+        for i in range(10):
+            for j in range(len(arr_new[i])):
+                store_arr.append(arr_new[i][j])
+        arr = store_arr
 
     return arr
 
