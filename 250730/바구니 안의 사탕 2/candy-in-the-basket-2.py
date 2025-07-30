01940f901a253e15ee798a14cdbeea115273d145
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-30 13:18:31 +0900

    Title: 바구니 안의 사탕 2, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 250730/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250730/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,15 +1,16 @@
 N, K = map(int, input().split())
 fin=0
 
-bag=[0]*100
+bag=[0]*101
 for i in range(N):
     candy,point=map(int,input().split())
-    bag[point-1]+=candy
+    bag[point]+=candy
 
-for i in range(100-K):
+for i in range(101):
     ans=0
     for j in range(i-K,i+K+1):
-        ans+=bag[j]
+        if 0<=j<=100:
+            ans+=bag[j]
     fin=max(fin,ans)
 print(fin)
 
