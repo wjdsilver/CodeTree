9e263a31e391ce8034ee7d7c14c2b4d47e55718e
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-30 16:57:46 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250730/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 250730/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -5,12 +5,28 @@ if a<=2:
     a1=0
 elif a>2:
     a1=a-3
+if N>=a+2:
+    a2=a+2
+elif N<a+2:
+    a2=N
+
 if b<=2:
     b1=0
 elif b>2:
     b1=b-3
+if N>=b+2:
+    b2=b+2
+elif N<b+2:
+    b2=N
+
 if c<=2:
     c1=0
 elif c>2:
     c1=c-3
-print(N**3-((N-a-2+a1)*(N-b-2+b1)*(N-c-2+c1)))
+if N>=c+2:
+    c2=c+2
+elif N<c+2:
+    c2=N
+
+#print(a1,b1,c1)
+print(N**3-((N-a2+a1)*(N-b2+b1)*(N-c2+c1)))
