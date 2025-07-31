3284b54899797231a74ba20d2b9a0ca1c8a750ad
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-31 16:53:58 +0900

    Title: 숫자 카운트, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250731/숫자 카운트/numeric-count.py
+++ 250731/숫자 카운트/numeric-count.py
@@ -10,8 +10,12 @@ for _ in range(N):
     c.append(c2)
 
 for n in range(100,1000):
-    if n%10 != n//10%10 and n//10%10 != n//100 and n//100 != n%10 and n//10%10 != 0 and n//100 != 0:
-        arr.append(str(n))
+    #if n%10 != n//10%10 and n//10%10 != n//100 and n//100 != n%10 and n//10%10 != 0 and n//100 != 0:
+    s = str(n)
+    if '0' in s:
+        continue
+    if len(set(s)) == 3:
+        arr.append(s)
 
 for n in arr:
     ok=True
