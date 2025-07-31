04f706c62589e6cd2aa107202a20a47d62c728e9
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-07-31 16:49:54 +0900

    Title: 숫자 카운트, Time: 53ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250731/숫자 카운트/numeric-count.py
@@ -0,0 +1,33 @@
+N = int(input())
+a, b, c = [], [], []
+ans=0
+arr=[]
+
+for _ in range(N):
+    num, c1, c2 = map(int, input().split())
+    a.append(str(num))
+    b.append(c1)
+    c.append(c2)
+
+for n in range(100,1000):
+    if n%10 != n//10%10 and n//10%10 != n//100 and n//100 != n%10 and n//10%10 != 0 and n//100 != 0:
+        arr.append(str(n))
+
+for n in arr:
+    for m in range(len(a)):
+        cnt1=0
+        cnt2=0
+        ok=True
+        for i in range(3):
+            if n[i]==a[m][i]:
+                cnt1+=1
+            elif n[i] in a[m]:
+                cnt2+=1
+        if cnt1 != b[m] or cnt2 != c[m]:
+            ok = False
+            break
+    if ok:
+        ans += 1
+print(ans)
+
+
