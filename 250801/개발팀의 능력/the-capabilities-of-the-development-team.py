595dfb9928fb3e2b19b51f80d891e5079de8e02a
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-01 10:53:18 +0900

    Title: 개발팀의 능력, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250801/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,18 @@
+import sys
+
+arr = list(map(int, input().split()))
+ans=sys.maxsize
+
+for i in arr:
+    team1=i
+    for j in range(4):
+        for k in range(j+1,5):
+            if arr[j]!=i and arr[k]!=i:
+                team2=arr[j]+arr[k]
+                team3=sum(arr)-team1-team2
+                if team1!=team2 and team1!=team3 and team2!=team3:
+                    M=max(team1,team2,team3)
+                    m=min(team1,team2,team3)
+                    ans=min(ans,M-m)
+print(ans)
+        
