3e3376c0a0593e63fdaa1a44a0ac7a77f649abda
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-01 15:15:35 +0900

    Title: 퀵 정렬 구현, Time: 160ms, Memory: 24MB, Status: Passed - Codetree

---
+++ 250801/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,27 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+        
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+  
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i],arr[j]=arr[j],arr[i]
+      
+    arr[i+1],arr[high]=arr[high],arr[i + 1]
+    return i + 1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pos = partition(arr, low, high)
+    
+        quick_sort(arr, low, pos - 1)
+        quick_sort(arr, pos + 1, high)
+
+quick_sort(arr,0,n-1)
+
+for elem in arr:
+    print(elem,end=" ")
