d948a65534295039c59e5dcd6b44d186455d003d
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-05 15:26:22 +0900

    Title: 스승의 은혜 3, Time: 81ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250805/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250805/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,19 +1,19 @@
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
-P = [gift[0] for gift in gifts]
-S = [gift[1] for gift in gifts]
+gifts.sort()
 
 ans=0
 
 for i in range(N):
-    cost = P[:]
+    cost = [g[0] for g in gifts]
+    deliver = [g[1] for g in gifts]
     cost[i] //= 2
 
     total = 0
     count = 0
     for j in range (len(cost)):
-        if total + cost[j] +S[j] <= B:
-            total = total +cost[j] +S[j]
+        if total + cost[j] +deliver[j] <= B:
+            total = total +cost[j] +deliver[j]
             count += 1
         else:
             break
