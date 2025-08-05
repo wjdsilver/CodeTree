51b6e17483f2f9c4de2be62ff391979c4e2b9507
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-05 15:17:56 +0900

    Title: 상해버린 치즈, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 250805/상해버린 치즈/rotten-cheese.py
+++ 250805/상해버린 치즈/rotten-cheese.py
@@ -1,24 +1,38 @@
 N, M, D, S = map(int, input().split())
 ans=0
 
-p, m, t = [], [], []
-
+eat=[]
 for _ in range(D):
     person, cheese, time = map(int, input().split())
-    p.append(person)
-    m.append(cheese)
-    t.append(time)
+    eat.append((person, cheese, time))
 
-sick_p, sick_t = [], []
+sick= []
 for _ in range(S):
     person, time = map(int, input().split())
-    sick_p.append(person)
-    sick_t.append(time)
+    sick.append((person, time))
 
-for i in range(1,M):
-    sick=0
-    if sick_t[sick_p.index(p[m.index(i)])]>t[m.index(i)]:
-        sick=m.count(i)
-    ans=max(ans,sick)
+for i in range(1,M+1):
+    log=[]
+    for person, c, time in eat:
+        if c == i:
+            log.append((person, time))
+
+    possible = True
+    for sp, st in sick:
+        found = False
+        for ep, et in log:
+            if sp == ep and et < st:
+                found = True
+                break
+        if not found:
+            possible = False
+            break
+
+    if possible:
+        people = set()
+        for person, c, time in eat:
+            if c == i:
+                people.add(person)
+        ans = max(ans, len(people))
 print(ans)
 
