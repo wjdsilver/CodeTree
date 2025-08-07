28868e21c822a63f93c6aa790037f7173c7717b5
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-07 14:40:51 +0900

    Title: 빙산의 일각 2, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250807/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 250807/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -3,7 +3,9 @@ h = [int(input()) for _ in range(n)]
 
 ans=0
 for i in range(1,max(h)):#해수면 높이
-    cnt=1
+    cnt=0
+    if h[0]-i>0:
+        cnt+=1
     for j in range(1,len(h)):
         if h[j-1]-i<=0 and 0<h[j]-i:
             cnt+=1
