69b0bfaa404e0963ff8672f93fae0c477200207e
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-11 16:17:34 +0900

    Title: 팀으로 하는 틱택토 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250811/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,34 @@
+inp = [list(map(int, list(input().strip()))) for _ in range(3)]
+cnt=0
+
+for i in range(1,10):
+    for j in range(1,10):
+        if i!=j:
+            TTT=False
+            for n in range(3):
+                if inp[n][0]==i:
+                    if inp[n][1]==i and inp[n][2]==j:
+                        TTT=True
+                    elif inp[n][1]==j:
+                        if inp[n][2]==i or inp[n][2]==j:
+                            TTT=True
+            for n in range(3):
+                if inp[0][n]==i:
+                    if inp[1][n]==i and inp[2][n]==j:
+                        TTT=True
+                    elif inp[1][n]==j:
+                        if inp[2][n]==i or inp[2][n]==j:
+                            TTT=True
+            if inp[0][0]==i:
+                if inp[1][1]==i and inp[2][2]==j:
+                    TTT=True
+                if inp[1][1]==j and inp[2][2]==i:
+                    TTT=True
+            if inp[0][2]==i:
+                if inp[1][1]==i and inp[2][0]==j:
+                    TTT=True
+                if inp[1][1]==j and inp[2][0]==i:
+                    TTT=True
+            if TTT==True:
+                cnt+=1
+print(cnt)
