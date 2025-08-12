022bc58e19c5f6cb991de7823f7e4ba5355341dd
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-12 16:08:32 +0900

    Title: 원소 값들의 최대 합, Time: 50ms, Memory: 17MB, Status: Passed - Codetree

--- 250812/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 250812/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -2,11 +2,12 @@ n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 fin=0
 
-for i in range(n):#시작위치
+for i in range(1,n+1):#시작위치
+    pos=i
     ans=0
     for _ in range(m):
-        ans += arr[n]
-        n = arr[n]
+        ans += arr[pos]
+        pos = arr[pos]
     fin=max(ans,fin)
 print(fin)
         
