5b238a24743038591ad368c4a5f364d19f6c2f52
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-08-12 15:12:30 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250812/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250812/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -2,10 +2,16 @@ N = int(input())
 str = input()
 ans=0
 
-for i in range(N//2+1):
-    sub=str[:i+1] 
-    if str.count(sub)==2:
-        ans=i+2
-    else:
+for i in range(N):
+    s=False
+    for j in range(N):
+        if i+j+1>N:
+            break
+        sub=str[j:i+j+1] 
+        if str.count(sub)==2:
+            s=True
+            ans=i+2
+            break
+    if s==False:
         break
 print(ans)
