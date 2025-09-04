71d1fa77e89db8ee49533036f133f4fa4223f59c
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-04 15:28:27 +0900

    Title: 이상한 폭탄 3, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/이상한 폭탄 3/strange-bomb-3.py
+++ 250904/이상한 폭탄 3/strange-bomb-3.py
@@ -1,18 +1,20 @@
 N, K = map(int, input().split())
 num = [int(input()) for _ in range(N)]
 ans=0
-for i in range(1,max(num)+1):
+for i in range(min(num),max(num)+1):
     new_arr=[]
     for n,elem in enumerate(num):
         if elem==i:
             new_arr.append(n)
-    valid = True
-    for j in range(2, len(new_arr)-1):
-        if new_arr[j] - new_arr[j-1] <= K or new_arr[j+1]-new_arr[j]<=K:
-            continue
-        else:
-            valid=False
-            break
+
+    valid = False
+    for j in range(len(new_arr)):
+        if j<len(new_arr)-1 and new_arr[j+1] - new_arr[j] <= K:
+            valid=True
+        if j>0 and new_arr[j]-new_arr[j-1]<=K:
+            valid=True
+
+            
 
     if valid:
         if num.count(i)>=2:
