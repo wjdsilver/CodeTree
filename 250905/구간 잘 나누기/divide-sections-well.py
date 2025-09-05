e23626e28c3f5907eca3db4278234aff257f5d91
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-05 16:54:41 +0900

    Title: 구간 잘 나누기, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250905/구간 잘 나누기/divide-sections-well.py
+++ 250905/구간 잘 나누기/divide-sections-well.py
@@ -1,21 +1,19 @@
-N, M = map(int, input().split())
-a = list(map(int, input().split()))
+N, M = map(int, input().split()) 
+a = list(map(int, input().split())) 
+minN=2*sum(a)//M 
+fin=1000 
 
-minN=2*sum(a)//M
-fin=1000
-for x in range(min(a),minN):#구간 합 기준?
-    arr=[]
-    tmp=0
-    for n in a:
-        tmp+=n
-        if tmp>=x:
-            arr.append(tmp)
-            tmp=0
-        else:
-            continue
+for x in range(max(a),sum(a)):#구간 합 기준? 
+    arr=[] 
+    tmp=0 
+    for n in a: 
+        if tmp+n>x: 
+            arr.append(tmp) 
+            tmp=n 
+        else: 
+            tmp+=n
     if tmp!=0:
-        arr.append(tmp)
+        arr.append(tmp) 
     if len(arr)==M:
         fin=min(max(arr),fin)
 print(fin)
-        
