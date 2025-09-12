9b91b85a98f9a9bb3aebfcfa596299f2b3f2fd33
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-12 10:48:30 +0900

    Title: 최대 H 점수 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250912/최대 H 점수 2/maximum-h-score-2.py
+++ 250912/최대 H 점수 2/maximum-h-score-2.py
@@ -2,15 +2,19 @@ N, L = map(int, input().split())
 a = list(map(int, input().split()))
 h=0
 
-for i in range(min(min(a),N),N+1):
-    cnt=0
-    hNum=0
+for i in range(min(min(a),N),N+1):#i는 H점수 후보
+    cnt=0 #H-1인거(값을 1 올려 H가 될수 있는거)
+    hNum=0 #숫자가 H이상인거
     for j in (a):
         if j==i-1:
             cnt+=1
         if j>=i:
             hNum+=1
 
-    if hNum+cnt>i or (hNum+cnt==i and cnt<=L):
-        h=i
+    if cnt<=L:
+        if  hNum+cnt>=i:
+            h=i
+    if cnt>L:
+        if hNum+L>=i:
+            h=i
 print(h)
