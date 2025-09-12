000c40a1bd0c84197cef09a9b5aeab1c4d3fd777
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-12 14:29:10 +0900

    Title: 두 직사각형, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250912/두 직사각형/two-rectangles.py
+++ 250912/두 직사각형/two-rectangles.py
@@ -1,12 +1,17 @@
 x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
-intersect=False
+intersect=True
 
-if a1<=x2 or b1<=y2:
-    intersect=True
-if x1<=a2 or y1<=b2:
-    intersect=True
+if a1>x2:
+    intersect=False
+if x1>a2:
+    intersect=False
+if b2<y1:
+    intersect=False
+if y2<b1:
+    intersect=False
+    
 if intersect==True:
     print("overlapping")
 else:
