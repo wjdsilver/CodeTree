a9dc4fa1a8bbd4ea3361cb6bb7a4e420363e4962
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-12 10:27:24 +0900

    Title: 최대 H 점수 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250912/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,16 @@
+N, L = map(int, input().split())
+a = list(map(int, input().split()))
+h=0
+
+for i in range(min(a),N+1):
+    cnt=0
+    hNum=0
+    for j in (a):
+        if j==i-1:
+            cnt+=1
+        if j>=i:
+            hNum+=1
+
+    if hNum+cnt>i or (hNum+cnt==i and cnt<=L):
+        h=i
+print(h)
