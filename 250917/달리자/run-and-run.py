67defe654071d59fbabe09ccc68335423d2ade85
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-17 16:18:40 +0900

    Title: 달리자, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250917/달리자/run-and-run.py
@@ -0,0 +1,24 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+ans=0
+
+for i in range(N,0,-1):
+    if B[i-1]-A[i-1]>=0:
+        B[i-1]=B[i-1]-A[i-1]
+        A[i-1]=0
+    else: 
+        A[i-1]=A[i-1]-B[i-1]
+        B[i-1]=0
+
+for j in range(0,N):
+    if A[j]>0:
+        for k in range(j,N):
+            if B[k]>0:
+                if B[k]>=A[j]:
+                    ans+=(k-j)*A[j]
+                    B[k]-=A[j]
+                if B[k]<A[j]:
+                    ans+=(k-j)*B[k]
+                    B[k]=0
+print(ans)
