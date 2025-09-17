20da71674f484170b369c0719cd61f2fb7d01999
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-17 16:30:28 +0900

    Title: 달리자, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250917/달리자/run-and-run.py
+++ 250917/달리자/run-and-run.py
@@ -12,13 +12,17 @@ for i in range(N,0,-1):
         B[i-1]=0
 
 for j in range(0,N):
-    if A[j]>0:
+    while A[j]>0:
         for k in range(j,N):
             if B[k]>0:
                 if B[k]>=A[j]:
                     ans+=(k-j)*A[j]
                     B[k]-=A[j]
+                    A[j]=0
                 if B[k]<A[j]:
                     ans+=(k-j)*B[k]
+                    A[j]-=B[k]
                     B[k]=0
+    else:
+        continue
 print(ans)
