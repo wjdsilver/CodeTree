9a27809ab23d55eed629e5028a216f8c487e21d2
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-18 15:07:02 +0900

    Title: 코딩톡, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250918/코딩톡/coding-talk.py
+++ 250918/코딩톡/coding-talk.py
@@ -4,11 +4,17 @@ messages = [tuple(input().split()) for _ in range(m)]
 c = [msg[0] for msg in messages]
 u = [int(msg[1]) for msg in messages]
 ppl=[(0)for _ in range(n)]
+
 for i in range(p,m):
     ppl[ord(c[i])-ord('A')]=1
-for j in range(n):
+
+for j in range(p):
+    if u[j]==u[p]:
+        ppl[ord(c[j])-ord('A')]=1
+
+for k in range(n):
     if ppl.count(0)>=u[p]:
         if u[p]==0:
             break
-        if ppl[j]==0:
-            print(chr(j+ord("A")),end=" ")
+        if ppl[k]==0:
+            print(chr(k+ord("A")),end=" ")
