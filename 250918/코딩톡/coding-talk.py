aef29879711f096c4ec95ccdf34a2d58ae365ea7
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-18 14:04:17 +0900

    Title: 코딩톡, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250918/코딩톡/coding-talk.py
@@ -0,0 +1,11 @@
+n, m, p = map(int, input().split())
+p=p-1
+messages = [tuple(input().split()) for _ in range(m)]
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
+ppl=[(0)for _ in range(n)]
+for i in range(p,m):
+    ppl[ord(c[i])-ord('A')]=1
+for j in range(n):
+    if ppl[j]==0:
+        print(chr(j+ord("A")),end=" ")
