f5a5e59009e50b29738958979abf498c96683706
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-25 15:20:15 +0900

    Title: L, R 그리고 B, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250925/L, R 그리고 B/l-r-and-b.py
+++ 250925/L, R 그리고 B/l-r-and-b.py
@@ -3,9 +3,9 @@ board = [list(input()) for _ in range(10)]
 for i in range(10):
     if "B" in board[i]:
         br=i
-    elif "L" in board[i]:
+    if "L" in board[i]:
         lr=i
-    elif "R" in board[i]:
+    if "R" in board[i]:
         rr=i
 
 for j in range(len(board[br])):
