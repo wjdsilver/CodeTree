9632b0919e21915f625e120d519b8b810811d200
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-25 14:25:33 +0900

    Title: ABC 줄 세우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250925/ABC 줄 세우기/abc-line-up.py
+++ 250925/ABC 줄 세우기/abc-line-up.py
@@ -1,12 +1,13 @@
 n = int(input())
 arr = list(input().split())
 cnt=0
-
-for i in range(n-1):
-    for j in range(i,n-1):
-        if arr[j]>arr[j+1]:
-            tmp=arr[j]
-            arr[j]=arr[j+1]
-            arr[j+1]=tmp
-            cnt+=1
+arr_sorted=sorted(arr)
+while arr_sorted!=arr:
+    for i in range(n-1):
+        for j in range(i,n-1):
+            if arr[j]>arr[j+1]:
+                tmp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=tmp
+                cnt+=1
 print(cnt)
