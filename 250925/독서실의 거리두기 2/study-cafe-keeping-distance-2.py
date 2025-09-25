308ea22f713a08920be0c11239164052285c6905
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-25 10:38:35 +0900

    Title: 독서실의 거리두기 2, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250925/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,32 @@
+N = int(input())
+seats = list(input())
+seat_candidate=[0]*N
+ans=N
+first,last=0,0
+
+for i in range(N):
+    if seats[i]==1:
+        if seat_candidate[0]==0:
+            first=i
+        for j in range(i+1,N):
+            if seats[j]==1:
+                seat_candidate[i]=j-i#seat_candidate에 다음 1까지의 거리 저장
+                break
+        if seat_candidate[N-1]==0:
+            last=N-i-1
+
+if max(max(seat_candidate)//2,first,last)==seat_candidate//2:
+    seats[(seat_candidate.index(max(seat_candidate)))+max(seat_candidate)//2]=1
+elif max(max(seat_candidate)//2,first,last)==first:
+    seats[0]=1
+elif max(max(seat_candidate)//2,first,last)==last:
+    seats[N-1]=1
+
+for i in range(N):
+    if seats[i]==1:
+        for j in range(i+1,N):
+            if seats[j]=="1":
+                ans=min(ans,j-i)
+                break
+
+print(ans)
