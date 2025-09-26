3073fb600abef88dfc4d86382be42a8a14263c40
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-26 16:06:40 +0900

    Title: 세 수의 최대 곱, Time: 83ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250926/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+
+ppp=arr[n-1]*arr[n-2]*arr[n-3] #셋다 양수
+pnn=arr[0]*arr[1]*arr[n-1] #하나양수 두개 절대값큰음수
+nnn=0 #셋다 절대값 작은 음수
+
+if 0 in arr: 
+    nnn=0
+else:
+    for i in range(n):
+        if arr[i]<0:
+            new_arr.append(arr[i])
+    new_arr.sort()
+    if len(new_arr)>=3:
+        nnn=new_arr[-1]*new_arr[-2]*new_arr[-3]
+print(max(ppp,pnn,nnn))
