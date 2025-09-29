f4c19f1b15208c001def58b3ec1da04dd2348c8e
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-29 15:25:05 +0900

    Title: 수열의 순서 바꾸기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250929/수열의 순서 바꾸기/reorder-sequence.py
+++ 250929/수열의 순서 바꾸기/reorder-sequence.py
@@ -3,7 +3,9 @@ sequence = list(map(int, input().split()))
 cnt=0
 
 for i in range(0,n-1):
-    if sequence[i:] is not sorted:
+    if sequence[i:]==sorted(sequence[i:]):
+        continue
+    else:
         cnt+=1
     
 print(cnt)
