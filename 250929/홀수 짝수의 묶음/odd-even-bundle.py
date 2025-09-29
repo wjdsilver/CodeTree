ed6f72e9fe9d2fcf707c03b7cd9358e5e25401e7
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-29 16:24:44 +0900

    Title: 홀수 짝수의 묶음, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250929/홀수 짝수의 묶음/odd-even-bundle.py
+++ 250929/홀수 짝수의 묶음/odd-even-bundle.py
@@ -13,11 +13,11 @@ if even==odd or even==odd+1:#짝홀짝홀이거나 짝홀짝
     ans=even+odd
 else:
     if min(even,odd)==even:#홀수가 더 많을때는 합쳐서 짝수를 만들수 있으나
-        if (odd-even)%3==0:
+        if (odd-even)%3==0:#짝홀 쌍 만들고 남은 홀수*2 홀수 로 짝홀 만들기
             print(2*even+(odd-even)//3*2)
-        elif (odd-even)%3==1:
+        elif (odd-even)%3==1:#짝홀 쌍 만들고 남은 홀수*4 홀수 홀수*2로 짝홀짝 만들기
             print(2*even+(odd-even-4)//3*2+1)
-        elif (odd-even)%3==2:
+        elif (odd-even)%3==2:#짝홀 쌍 만들고 남은 홀수*2 홀수 홀수*2로 짝홀짝 만들기
             print(2*even+(odd-even-2)//3*2+1)
 
     else:#짝수가 더 많을때는 합쳐서 홀수를 만들수 없음.
