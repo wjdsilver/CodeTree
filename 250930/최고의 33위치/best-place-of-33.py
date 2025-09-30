bef12f8d9e34972952ffdb4443e3511b4e125503
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-09-30 17:34:12 +0900

    Title: 최고의 33위치, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250930/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,13 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+ans=0
+
+for i in range(n-2):#좌상단
+    for j in range(n-2):#좌상단
+        cnt=0
+        for k in range(3):
+            for m in range(3):
+                if grid[i+k][j+m]==1:
+                    cnt+=1
+    ans=max(ans,cnt)
+print(ans)
