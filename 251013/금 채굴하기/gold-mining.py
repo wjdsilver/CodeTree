26cdbf3b1b8313f41fa2460da440f106f33273a0
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-10-13 13:43:51 +0900

    Title: 금 채굴하기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251013/금 채굴하기/gold-mining.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+
+def casek(i, j, k):
+    cnt = 0
+    for a in range(i - k, i + k + 1):
+        for b in range(j - k, j + k + 1):
+            if 0 <= a < n and 0 <= b < n:
+                if i + j - k <= a + b <= i + j + k:
+                    if grid[a][b] == 1:
+                        cnt += 1
+    if 0 < (cnt * m - (k**2 + (k+1)**2)):
+        return cnt
+    return 0
+
+for i in range(n):
+    for j in range(n):
+        for k in range((n + 1) // 2):
+            ans = max(ans, casek(i, j, k))
+
+print(ans)
