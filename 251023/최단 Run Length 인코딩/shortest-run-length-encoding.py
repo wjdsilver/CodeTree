f38aeb309323432089838a628a70dbd5b6db0930
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-10-23 16:07:48 +0900

    Title: 최단 Run Length 인코딩, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251023/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,37 @@
+A = input()
+ans=len(A)
+
+def shift(A):
+    A=A[-1]+A[:-1]
+    return A[-1]+A[:-1]
+
+def RLE(A):
+    result=[]
+    cnt=1
+    for i in range(len(A)-1):
+        if A[i]==A[i+1]:
+            cnt+=1
+            continue
+        else:
+            result.append(A[i])
+            if 10>cnt:
+                result.append(cnt)
+            else:
+                result.append(cnt//10)
+                result.append(cnt%10)
+            cnt=1
+    if cnt==len(A):
+        result.append(A[i])
+        if 10>cnt:
+            result.append(cnt)
+        else:
+            result.append(cnt//10)
+            result.append(cnt%10)
+    return len(result)
+
+for i in range(len(A)):
+    shift(A)
+    ans=min(ans,RLE(A))
+
+print(ans)
+
