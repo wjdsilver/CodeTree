314aa57e47b704e79b7b30c787edc1b805cc3206
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-10-23 16:58:12 +0900

    Title: 최단 Run Length 인코딩, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251023/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 251023/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,35 +1,26 @@
 A = input()
 ans=len(A)*2
 
-def shift(A):
-    A=A[-1]+A[:-1]
-    return A
+def shift(y):
+    return y[-1]+y[:-1]
+    
 
-def RLE(A):
-    result=[]
-    cnt=1
-    for i in range(len(A)-1):
-        if A[i]==A[i+1]:
-            cnt+=1
-            continue
+def RLE(x):
+    result = []
+    cnt = 1
+    for i in range(len(x)-1):
+        if x[i] == x[i+1]:
+            cnt += 1
         else:
-            result.append(A[i])
-            if 10>cnt:
-                result.append(cnt)
-            else:
-                result.append(cnt//10)
-                result.append(cnt%10)
-            cnt=1
-    if cnt==len(A):
-        result.append(A[0])
-        if 10>cnt:
-            result.append(cnt)
-        else:
-            result.append(cnt//10)
-            result.append(cnt%10)
-    return len(result)
+            result.append(x[i])
+            result.append(str(cnt))
+            cnt = 1
+    result.append(x[-1])
+    result.append(str(cnt))
+    return len(''.join(result))
 
-for i in range(len(A)):
-    ans=min(ans,RLE(shift(A)))
+for i in range(len(A)+1):
+    A=shift(A)
+    ans=min(ans,RLE(A))
 
 print(ans)
