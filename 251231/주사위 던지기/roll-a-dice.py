700e19bb65c4099a37662d268419ab432ac4c6d6
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2025-12-31 15:32:33 +0900

    Title: 주사위 던지기, Time: 85ms, Memory: 19MB, Status: Passed - Codetree

--- 251231/주사위 던지기/roll-a-dice.py
+++ 251231/주사위 던지기/roll-a-dice.py
@@ -3,29 +3,31 @@ directions = list(input().split())
 r-=1
 c-=1
 LMROUD=[4,6,3,1,5,2]#왼,본인,오,마주본,위,아래
+dr = {'L':0, 'R':0, 'U':-1, 'D':1}
+dc = {'L':-1, 'R':1, 'U':0, 'D':0}
 
 grid=[[0]*n for _ in range (n)]
 grid[r][c] = LMROUD[1]
 
 def move(x):
     global r,c,LMROUD
+    nr = r + dr[directions[x]]
+    nc = c + dc[directions[x]]
+
+    if not (0 <= nr < n and 0 <= nc < n):
+        return
     if directions[x]=="L":
         LMROUD=[LMROUD[3]]+LMROUD[:3]+LMROUD[4:]
-        c-=1
     elif directions[x]=="U":
         LMROUD=[LMROUD[0]]+[LMROUD[4]]+[LMROUD[2]]+[LMROUD[5]]+[LMROUD[3]]+[LMROUD[1]]
-        r-=1
     elif directions[x]=="R":
         LMROUD=LMROUD[1:4]+[LMROUD[0]]+LMROUD[4:]
-        c+=1
     elif directions[x]=="D":
         LMROUD=[LMROUD[0]]+[LMROUD[5]]+[LMROUD[2]]+[LMROUD[4]]+[LMROUD[1]]+[LMROUD[3]]
-        r+=1
-    return LMROUD
+    r,c=nr,nc
+    grid[r][c] = LMROUD[1]
 
 for i in range (m):#m번에 걸쳐서
     move(i)
-    if 0 <= r < n and 0 <= c < n:
-        grid[r][c] = LMROUD[1]
 
 print(sum(sum(row) for row in grid))
