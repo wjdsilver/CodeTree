d69613e9a78aa5d9872a1b3847686bfb42d8a1c2
wjdsilver_113 <104892553+wjdsilver@users.noreply.github.com>
2026-01-21 17:44:32 +0900

    Title: 벽이 없는 충돌 실험, Time: 350ms, Memory: 24MB, Status: Passed - Codetree

--- 260121/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 260121/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,15 +1,13 @@
 #구슬의 번호와 무게 주어짐. 2초에 한칸씩 이동
-#각 테스트 케이스마다 모든 구슬을 보면 시간이 너무 많이 걸림... 
+#각 테스트 케이스마다 모든 구슬을 보면 시간이 너무 많이 걸림->충돌케이스만 살피기
+#꼭 다시 풀어볼것
+
 T = int(input())#총 테스트케이스
 
 for _ in range(T): #각 테스트케이스마다
-    t=0
     ans=-1
-    winner =0
     N = int(input()) #구슬 개수
     x, y, w, dr, dc, idx = [], [], [], [], [], [] #좌표, 무게, 방향
-    alive = [True] * N
-    alive_cnt = N
 
     #N개의 구슬의 좌표, 무게, 이동방향
     for i in range(N):
@@ -26,37 +24,92 @@ for _ in range(T): #각 테스트케이스마다
             dr.append(-1); dc.append(0)
         elif di == "R":
             dr.append(1); dc.append(0)
-    testtime=max(x)-min(x)+max(y)-min(y)
-    for _ in range(testtime):
-        if alive_cnt <= 1:
-            break
-        #모든 구슬 이동
-        for i in range(N):
-            if alive[i]:
-                x[i] += dr[i]
-                y[i] += dc[i]
-        t+=1
-        pos_dict = {}
-        for i in range(N):
-            if not alive[i]:
-                continue
-            p = (x[i], y[i])
-            if p not in pos_dict:
-                pos_dict[p] = []
-            pos_dict[p].append(i)
 
-        for group in pos_dict.values(): 
-            if len(group) >= 2: 
-                ans = t 
-                winner = group[0] 
-                for i in group[1:]: 
-                    if w[i] > w[winner] or (w[i] == w[winner] and idx[i] > idx[winner]): 
-                        winner = i 
-                for i in group: 
-                    if i != winner and alive[i]:
-                        alive[i] = False
-                        alive_cnt -= 1
-         
-    
+
+    events = []
+
+    #세로축충돌
+    U = {}
+    D = {}
+
+    for i in range(N):
+        if dr[i] == 0 and dc[i] == 1:  # U
+            if x[i] not in U:
+                U[x[i]] = []
+            U[x[i]].append(i)
+        elif dr[i] == 0 and dc[i] == -1:  # D
+            if x[i] not in D:
+                D[x[i]] = []
+            D[x[i]].append(i)
+
+    for xx in U:
+        if xx in D:
+            for u in U[xx]:
+                for d in D[xx]:
+                    if y[u] < y[d]:
+                        t = (y[d] - y[u]) // 2
+                        events.append((t, u, d))
+
+    # 가로축 충돌
+    L = {}
+    R = {}
+
+    for i in range(N):
+        if dr[i] == -1:  # L
+            if y[i] not in L:
+                L[y[i]] = []
+            L[y[i]].append(i)
+        elif dr[i] == 1:  # R
+            if y[i] not in R:
+                R[y[i]] = []
+            R[y[i]].append(i)
+
+    for yy in L:
+        if yy in R:
+            for l in L[yy]:
+                for r in R[yy]:
+                    if x[r] < x[l]:
+                        t = (x[l] - x[r]) // 2
+                        events.append((t, r, l))
+
+    # 대각선 위치
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                continue
+
+            # RU
+            if dr[i] == 1 and dc[i] == 0 and dr[j] == 0 and dc[j] == 1:
+                if x[i] < x[j] and y[i] > y[j] and x[j] - x[i] == y[i] - y[j]:
+                    events.append((x[j] - x[i], i, j))
+
+            # RD
+            if dr[i] == 1 and dc[i] == 0 and dr[j] == 0 and dc[j] == -1:
+                if x[i] < x[j] and y[i] < y[j] and x[j] - x[i] == y[j] - y[i]:
+                    events.append((x[j] - x[i], i, j))
+
+            # LU
+            if dr[i] == -1 and dc[i] == 0 and dr[j] == 0 and dc[j] == 1:
+                if x[i] > x[j] and y[i] > y[j] and x[i] - x[j] == y[i] - y[j]:
+                    events.append((x[i] - x[j], i, j))
+
+            # LD
+            if dr[i] == -1 and dc[i] == 0 and dr[j] == 0 and dc[j] == -1:
+                if x[i] > x[j] and y[i] < y[j] and x[i] - x[j] == y[j] - y[i]:
+                    events.append((x[i] - x[j], i, j))
+
+    events.sort()
+    alive = [True] * N
+
+    for t, i, j in events:
+        if not alive[i] or not alive[j]:
+            continue
+
+        ans = t
+
+        if w[i] > w[j] or (w[i] == w[j] and idx[i] > idx[j]):
+            alive[j] = False
+        else:
+            alive[i] = False
 
     print(ans)
